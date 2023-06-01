36a9a77d47d320aae001eb354157fe2420fb67a2
riri8ml <riri8.ml.mailer@gmail.com>
2023-06-01 19:31:52 +0900

    Update
    - 핑크색 아이템1 추가 : 일정 시간 동안 검은 손의 추적이 느려짐
    - 파란색 아이템2 추가 : 캐릭터의 이동 속도가 잠시 동안 빨라짐
    - 보라색 아이템3 추가 : 1회성 더블 점프(스페이스바를 사용해 한번 더 점프 가능)

--- GameGraphic/Assets/Scripts/Components/BlackHand.cs
+++ GameGraphic/Assets/Scripts/Components/BlackHand.cs
@@ -6,32 +6,74 @@ using UnityEngine.Serialization;
 [AddComponentMenu("BlackHand")]
 public class BlackHand : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private float chasingSpd = 1;
-    //이 값이 작을 수록 난이도가 증가하며 범위는 0 < x <= 1
-    [SerializeField] private float chasingCefficient= 0.2f;
-    private static Rigidbody2D _rigid;
+	[SerializeField] private GameObject player;
+	[SerializeField] private float chasingSpd = 1;
+	//이 값이 작을 수록 난이도가 증가하며 범위는 0 < x <= 1
+	[SerializeField] private float chasingCefficient = 0.2f;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        _rigid = GetComponent<Rigidbody2D>();
-    }
+	private static Rigidbody2D _rigid;
+	//기존 난이도 저장
+	private float startChasingCefficient;
+	//난이도 감소 지속 시간
+	private float reduceDifficultyDuration = 2f;
+	//난이도 감소 타이머
+	private float reduceDifficultyTimer;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        ChasingPlayer();
-    }
+	//난이도가 감소 되었는지
+	private bool isReduceDifficulty;
 
-    private void ChasingPlayer()
-    {
-        var coefficient = player.transform.position.y - transform.position.y;
-        _rigid.velocity =
-            new Vector2()
-            {
-                x = 0,
-                y = Mathf.Log(coefficient,1 + chasingCefficient) * chasingSpd * Time.deltaTime 
-            };
-    }
+	// Start is called before the first frame update
+	void Awake()
+	{
+		_rigid = GetComponent<Rigidbody2D>();
+		//기존 난이도 저장
+		startChasingCefficient = chasingCefficient;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		//난이도 감소가 진행되고 있으면
+		if (isReduceDifficulty)
+		{
+			//타이머 시작
+			ReduceDifficultyOverTime();
+		}
+		ChasingPlayer();
+	}
+
+	private void ChasingPlayer()
+	{
+		var coefficient = player.transform.position.y - transform.position.y;
+		_rigid.velocity =
+			new Vector2()
+			{
+				x = 0,
+				y = Mathf.Log(coefficient, 1 + chasingCefficient) * chasingSpd * Time.deltaTime
+			};
+	}
+
+	//*난이도 감소 아이템 함수
+	public void ReduceDifficulty()
+	{
+		//난이도 감소 시작
+		chasingCefficient += 0.8f; //난이도 감소 값
+		isReduceDifficulty = true;
+
+		//타이머 초기화
+		reduceDifficultyTimer = 0f;
+	}
+
+	private void ReduceDifficultyOverTime()
+	{
+		reduceDifficultyTimer += Time.deltaTime;
+		//난이도 감소 타이머가 지정한 지속시간까지 가면
+		if (reduceDifficultyTimer >= reduceDifficultyDuration)
+		{
+			//난이도 감소 끝
+			isReduceDifficulty = false;
+			//난이도 다시 원래상태로 복구
+			chasingCefficient = startChasingCefficient;
+		}
+	}
 }
--- GameGraphic/Assets/Scripts/Components/ObjectManager.cs
+++ GameGraphic/Assets/Scripts/Components/ObjectManager.cs
@@ -5,103 +5,141 @@ using UnityEngine;
 [AddComponentMenu("ObjectManager")]
 public class ObjectManager : MonoBehaviour
 {
-    //기본 구름
-    [SerializeField] private GameObject cloudGameObject;
+	//기본 구름
+	[SerializeField] private GameObject cloudGameObject;
 
-    //먹구름
-    [SerializeField] private GameObject darkCloudGameObject;
+	//먹구름
+	[SerializeField] private GameObject darkCloudGameObject;
 
-    //레인보우
-    [SerializeField] private GameObject rainbowGameObject;
+	//레인보우
+	[SerializeField] private GameObject rainbowGameObject;
 
-    //안개
-    [SerializeField] private GameObject fogGameObject;
+	//안개
+	[SerializeField] private GameObject fogGameObject;
 
-    //풍선
-    [SerializeField] private GameObject balloonGameObject;
+	//풍선
+	[SerializeField] private GameObject balloonGameObject;
 
-    //화살
-    [SerializeField] private GameObject arrowGameObject;
+	//화살
+	[SerializeField] private GameObject arrowGameObject;
 
-    //이전에 스폰된 구름 기록 저장용
-    private GameObject previousCloud;
+	//아이템1
+	[SerializeField] private GameObject item1GameObject;
 
-    // 오브젝트 사이 간격
-    [SerializeField] private float distanceBetweenGameObject = 3;
+	//아이템2
+	[SerializeField] private GameObject item2GameObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(SpawnArrow());
-    }
+	//아이템3
+	[SerializeField] private GameObject item3GameObject;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //랜덤 소환 값
-        int randomvalue = Random.Range(0, 100);
+	// 오브젝트 사이 간격
+	[SerializeField] private float distanceBetweenGameObject = 3;
 
-        // (ObjectManager의 y값)이 (플레이어 y값 + 오브젝트 간격 + 조정 수치)보다 낮을 경우 ObjectManager의 y좌표를 위로 올리고 오브젝트 소환
-        if (transform.position.y < GameManager.Instance.player.transform.position.y + distanceBetweenGameObject + 5)
-        {
-            transform.position = new Vector2(Random.Range(-8.0f,8.0f), (transform.position.y) + distanceBetweenGameObject);
+	//이전에 스폰된 오브젝트 기록
+	private GameObject previousObject;
 
-            //적은 확률로 소환되는 레인보우, 안개, 풍선, 먹구름
-            if (randomvalue >= 0 && randomvalue <= 15)
-            {
-                Instantiate(rainbowGameObject, transform.position, transform.rotation);
-                //소환한 오브젝트 기록
-                previousCloud = rainbowGameObject;
-            }
-            else if (randomvalue >= 16 && randomvalue <= 30)
-            {
-                Instantiate(fogGameObject, transform.position, transform.rotation);
-                //소환한 오브젝트 기록
-                previousCloud = fogGameObject;
-            }
-            else if (randomvalue >= 31 && randomvalue <= 45)
-            {
-                Instantiate(balloonGameObject, transform.position, transform.rotation);
-                //소환한 오브젝트 기록
-                previousCloud = balloonGameObject;
-            }
-            else if (randomvalue >= 46 && randomvalue <= 60)
-            {
-                // 이전에 소환된 오브젝트가 먹구름이면 기본 구름 소환 <- 먹구름만 두번 소환되어 못올라가는 경우 방지
-                if (previousCloud == darkCloudGameObject)
-                {
-                    Instantiate(cloudGameObject, transform.position, transform.rotation);
-                    //소환한 오브젝트 기록
-                    previousCloud = cloudGameObject;
-                }
-                else
-                {
-                    Instantiate(darkCloudGameObject, transform.position, transform.rotation);
-                    //소환한 오브젝트 기록
-                    previousCloud = darkCloudGameObject;
-                }
-            }
-            //나머지 확률은 기본구름 소환
-            else
-            {
-                Instantiate(cloudGameObject, transform.position, transform.rotation);
-                //소환한 오브젝트 기록
-                previousCloud = cloudGameObject;
-            }
-        }
-    }
 
-    IEnumerator SpawnArrow()
-    {
-        // 화살 스폰
-        yield return new WaitForSeconds(Random.Range(6.0f,6.0f));
-        Instantiate(arrowGameObject,
-            new Vector2(
-                GameManager.Instance.player.gameObject.transform.position.x,
-                GameManager.Instance.player.gameObject.transform.position.y-12),
-            transform.rotation
-            );
-        StartCoroutine(SpawnArrow());
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartCoroutine(SpawnArrow());
+	}
 
-    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		//랜덤 오브젝트 소환 값
+		int randomValue1 = Random.Range(0, 100);
+		int randomValue2 = Random.Range(0, 100);
+
+
+		// (ObjectManager의 y값)이 (플레이어 y값 + 오브젝트 간격 + 조정 수치)보다 낮을 경우 ObjectManager의 y좌표를 위로 올리고 오브젝트 소환
+		if (transform.position.y < GameManager.Instance.player.transform.position.y + distanceBetweenGameObject + 5)
+		{
+
+			transform.position = new Vector2(Random.Range(-8.0f, 8.0f), (transform.position.y) + distanceBetweenGameObject);
+
+			//아이템 오브젝트위로 배치
+			Vector2 itemPosition = transform.position;
+			itemPosition.y += 1f;
+
+
+			//적은 확률로 소환되는 레인보우, 안개, 풍선, 먹구름
+			if (randomValue1 >= 0 && randomValue1 <= 15)
+			{
+				Instantiate(rainbowGameObject, transform.position, transform.rotation);
+				//소환한 오브젝트 기록
+				previousObject = rainbowGameObject;
+			}
+			else if (randomValue1 >= 16 && randomValue1 <= 30)
+			{
+				Instantiate(fogGameObject, transform.position, transform.rotation);
+				//소환한 오브젝트 기록
+				previousObject = fogGameObject;
+			}
+			else if (randomValue1 >= 31 && randomValue1 <= 45)
+			{
+				Instantiate(balloonGameObject, transform.position, transform.rotation);
+				//소환한 오브젝트 기록
+				previousObject = balloonGameObject;
+			}
+			else if (randomValue1 >= 46 && randomValue1 <= 60)
+			{
+				// 이전에 소환된 오브젝트가 먹구름이면 기본 구름 소환 <- 먹구름만 두번 소환되어 못올라가는 경우 방지
+				if (previousObject == darkCloudGameObject)
+				{
+					Instantiate(cloudGameObject, transform.position, transform.rotation);
+					//소환한 오브젝트 기록
+					previousObject = cloudGameObject;
+				}
+				else
+				{
+					Instantiate(darkCloudGameObject, transform.position, transform.rotation);
+					//소환한 오브젝트 기록
+					previousObject = darkCloudGameObject;
+				}
+			}
+			//나머지 확률은 기본구름 소환
+			else
+			{
+				Instantiate(cloudGameObject, transform.position, transform.rotation);
+				//소환한 오브젝트 기록
+				previousObject = cloudGameObject;
+			}
+
+
+			//이전에 소환된 오브젝트가 먹구름 또는 풍선이 아니면 아이템 소환
+			if (previousObject != darkCloudGameObject && previousObject != balloonGameObject)
+			{
+				//적은 확률로 소환되는 아이템1,2,3
+				if (randomValue2 >= 0 && randomValue2 <= 8)
+				{
+					Instantiate(item1GameObject, itemPosition, transform.rotation);
+				}
+				else if (randomValue2 >= 30 && randomValue2 <= 38)
+				{
+					Instantiate(item2GameObject, itemPosition, transform.rotation);
+				}
+				else if (randomValue2 >= 60 && randomValue2 <= 68)
+				{
+					Instantiate(item3GameObject, itemPosition, transform.rotation);
+				}
+			}
+		}
+	}
+
+	IEnumerator SpawnArrow()
+	{
+		// 화살 스폰
+		yield return new WaitForSeconds(Random.Range(6.0f, 6.0f));
+		Instantiate(arrowGameObject,
+			new Vector2(
+				GameManager.Instance.player.gameObject.transform.position.x,
+				GameManager.Instance.player.gameObject.transform.position.y - 12),
+			transform.rotation
+			);
+		StartCoroutine(SpawnArrow());
+
+	}
 }
