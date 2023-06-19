9d17c0209f3788779056c77c41673540e867a548
lawsuit3310 <aimeet5480@gmail.com>
2023-06-19 23:34:20 +0900

    20230619

---
+++ GameGraphic/Assets/Video/StoryController.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class StoryController : MonoBehaviour
+{
+    public VideoPlayer video;
+
+    public bool isDone = false;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        video = video == null ? GetComponent<VideoPlayer>() : video;
+    }
+
+    private void Start()
+    {
+        video.loopPointReached +=
+            (VideoPlayer video) =>
+            {
+                isDone = true;
+            };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (video.time >= 4.7f && video.time <= 4.8f)
+        {
+            video.Pause();
+            video.time = 4.82f;
+        }
+        else if (video.time >= 7.7f && video.time <= 7.8f)
+        {
+            video.Pause();
+            video.time = 7.81f;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (isDone)
+                SceneManager.LoadScene(1);
+            else if (video.isPaused)
+            {
+                video.Play();
+            }
+        }
+    }
+}
